32aff3c29aee62640d7404b748cb4550ad494437
Joos <Julia.Joos@mfn-berlin.de>
2017-03-14 13:34:07 +0100

    first script

---
+++ fossil_tortoises.R
@@ -0,0 +1,12 @@
+#### Packages ####
+library(ggplot2) # for plots and graphs (cheat sheet available)
+library(dplyr) # for organizing data (data wrangling cheat sheet available)
+library(paleobioDB) #to load, visualize and process data from PDBD
+#The following object is masked from 'package:dplyr':  select
+library(speciesgeocodeR) # categorization of species occurrences for biodiversity, biogeography, ecology and evolution
+
+#### Data basis ####
+setwd("//naturkundemuseum-berlin.de/MuseumDFSRoot/Benutzer/Julia.Joos/Eigene Dateien/MA")
+
+ALL<-read.csv(choose.files(" "), skip = 17,sep=",", header=TRUE) # read csv from line 18 (skip), separated with comma (NOT WORKING)
+
