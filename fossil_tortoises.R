7d6a00f1ff6258f45a5c733ec1775d92429138be
Joos <Julia.Joos@mfn-berlin.de>
2017-03-16 17:22:39 +0100

    add paleoTS package

--- fossil_tortoises.R
+++ fossil_tortoises.R
@@ -4,6 +4,7 @@ library(dplyr) # for organizing data (data wrangling cheat sheet available)
 library(paleobioDB) #to load, visualize and process data from PDBD
 #The following object is masked from 'package:dplyr':  select
 library(speciesgeocodeR) # categorization of species occurrences for biodiversity, biogeography, ecology and evolution
+library(paleoTS) # analyze paleontological time-series
 
 #### Data basis ####
 setwd("//naturkundemuseum-berlin.de/MuseumDFSRoot/Benutzer/Julia.Joos/Eigene Dateien/MA")
