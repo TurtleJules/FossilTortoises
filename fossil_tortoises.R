3209ac765e19d229a00735c700bbec17e8622001
Joos <Julia.Joos@mfn-berlin.de>
2017-04-12 18:47:20 +0200

    organize references

--- fossil_tortoises.R
+++ fossil_tortoises.R
@@ -1,6 +1,8 @@
 #### Packages ####
 library(ggplot2) # for plots and graphs (cheat sheet available)
 library(dplyr) # for organizing data (data wrangling cheat sheet available)
+library(tidyr)
+library(stringi)
 library(paleobioDB) #to load, visualize and process data from PDBD
 #The following object is masked from 'package:dplyr':  select
 library(speciesgeocodeR) # categorization of species occurrences for biodiversity, biogeography, ecology and evolution
@@ -12,10 +14,29 @@ setwd("//naturkundemuseum-berlin.de/MuseumDFSRoot/Benutzer/Julia.Joos/Eigene Dat
 ### FosFarBase ####
 ALL<-read.csv(choose.files(" "), sep=";", header=TRUE)
 
+Ref <- ALL %>%
+  filter(CL == "-") %>%
+  tidyr::separate(Reference, c("Ref1", "Ref2", "Ref3", "Ref4", "Ref5", "Ref6", "Ref7", "Ref8", "Ref9"), sep=" or ") %>%
+  tidyr::gather(RefNr, Reference, Ref1, Ref2, Ref3, Ref4, Ref5, Ref6, Ref7, Ref8, Ref9, na.rm=TRUE) %>% 
+  tidyr::separate(Reference, c("AuthorYear", "TitleJournal", "Rest", "Rest2"), sep=":") %>%
+#  tidyr::separate(Ref2, c("Ref2", "Ref3"), sep=" or ") %>%
+#  tidyr::separate(AuthorYear, c("Author", "Year"), sep=",") %>%
+#  mutate(RefYear=Year)
+  mutate(RefYear=stri_sub(as.vector(AuthorYear), from = -5, to = -1)) %>%
+#  tidyr::separate(AuthorYear, c("Author", "Year"), "(?<=[a-z]) ?(?=[1-2])")
+#  mutate(Year = str_sub(Ref$AuthorYear,-5, -1)) #%>%
+  tidyr::unite(TitleJournal, TitleJournal, Rest, Rest2, sep=" ") %>%
+  tidyr::separate(stri_reverse(TitleJournal), c("Title", "Journal", "rest", "rest2" ), sep="(?<=\\.)")
+
+# TO DO: Journal in eigene Spalte kriegen! #######
+#unique(Ref$RefNr[which(Ref$Locality == "Sandalja near Pula")])
+#Ref[6,29] # [line, column]
+
+AuthorYear <- as.vector(Ref$AuthorYear)
+Year <- stri_sub(AuthorYear, from = -5, to = -1)
+
 ALLCL <- ALL %>% 
-  filter(CL != "-") %>%
-  filter(!grepl("o.",gen_linien)) %>%
-  filter(gen_linien != "IX" & gen_linien != "VIII" & gen_linien != "X") # 3 Linien > 10 samples rausfiltern
+  filter(CL != "-")
 
 
 ### Checklist ####
