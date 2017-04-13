883c39ae401c8829bf2b3e27b366f40a14fbcb0a
Joos <Julia.Joos@mfn-berlin.de>
2017-04-13 18:31:07 +0200

    oganize references

--- fossil_tortoises.R
+++ fossil_tortoises.R
@@ -2,7 +2,7 @@
 library(ggplot2) # for plots and graphs (cheat sheet available)
 library(dplyr) # for organizing data (data wrangling cheat sheet available)
 library(tidyr)
-library(stringi)
+library(stringi) # process character strings
 library(paleobioDB) #to load, visualize and process data from PDBD
 #The following object is masked from 'package:dplyr':  select
 library(speciesgeocodeR) # categorization of species occurrences for biodiversity, biogeography, ecology and evolution
@@ -16,27 +16,56 @@ ALL<-read.csv(choose.files(" "), sep=";", header=TRUE)
 
 Ref <- ALL %>%
   filter(CL == "-") %>%
+  group_by(Country, Locality) %>%
   tidyr::separate(Reference, c("Ref1", "Ref2", "Ref3", "Ref4", "Ref5", "Ref6", "Ref7", "Ref8", "Ref9"), sep=" or ") %>%
   tidyr::gather(RefNr, Reference, Ref1, Ref2, Ref3, Ref4, Ref5, Ref6, Ref7, Ref8, Ref9, na.rm=TRUE) %>% 
   tidyr::separate(Reference, c("AuthorYear", "TitleJournal", "Rest", "Rest2"), sep=":") %>%
-#  tidyr::separate(Ref2, c("Ref2", "Ref3"), sep=" or ") %>%
-#  tidyr::separate(AuthorYear, c("Author", "Year"), sep=",") %>%
-#  mutate(RefYear=Year)
   mutate(RefYear=stri_sub(as.vector(AuthorYear), from = -5, to = -1)) %>%
 #  tidyr::separate(AuthorYear, c("Author", "Year"), "(?<=[a-z]) ?(?=[1-2])")
 #  mutate(Year = str_sub(Ref$AuthorYear,-5, -1)) #%>%
-  tidyr::unite(TitleJournal, TitleJournal, Rest, Rest2, sep=" ") %>%
-  tidyr::separate(stri_reverse(TitleJournal), c("Title", "Journal", "rest", "rest2" ), sep="(?<=\\.)")
+  tidyr::unite(TitleJournal, TitleJournal, Rest, Rest2, sep=" ")
+
+Lit <- Ref %>%
+  group_by(Country, Locality) %>%
+  select(Country, Locality, Latitude, Longitude, Epoch, Taxon, Author,comment, collection,
+         CL, RefNr, AuthorYear, RefYear, TitleJournal, note)
+
+
+write.csv(Lit, "//naturkundemuseum-berlin.de/MuseumDFSRoot/Benutzer/Julia.Joos/Eigene Dateien/MA/References.txt", fileEncoding = "UTF-16LE") #fileEncoding keeps column structure without havin to drop row names
+#, sep="\t"
+
+Referenc <- unique(Lit$TitleJournal)
+Reference<- na.omit(Referenc)
+write.table(Reference, "//naturkundemuseum-berlin.de/MuseumDFSRoot/Benutzer/Julia.Joos/Eigene Dateien/MA/Reference.txt",  sep="\t")
+
+# Errors
+# 1. Lit[367,14]  
+# 1. Lit$TitleJournal[which(Lit$Locality == "Cava Monticino, near Brisigella, Emilia-Romana")]  --> needs to be fixed (same as above)
+# 2. Lit$TitleJournal[which(Lit$Country == "Japan")]
+# 3. Lit$TitleJournal[which(Lit$Country == "Oman")]
+
+#new<-read.csv(choose.files(" "), header=T, sep=";")   #, header=TRUE
+new<-read.delim("ref.csv", header=T, sep=";")
+
+References <- Lit %>%
+  merge(new, by = "TitleJournal", all.x= TRUE) %>%
+  select(Country, Locality, Latitude, Longitude, Epoch, Taxon, Author,comment, collection,
+         CL, RefNr, AuthorYear, RefYear, TitleJournal, Journal, note) %>%
+  arrange(Country, Locality, RefNr)
+
+
+write.table(References, "//naturkundemuseum-berlin.de/MuseumDFSRoot/Benutzer/Julia.Joos/Eigene Dateien/MA/References_new1.txt",  sep="\t", row.names = FALSE)
 
 # TO DO: Journal in eigene Spalte kriegen! #######
 #unique(Ref$RefNr[which(Ref$Locality == "Sandalja near Pula")])
 #Ref[6,29] # [line, column]
 
-AuthorYear <- as.vector(Ref$AuthorYear)
-Year <- stri_sub(AuthorYear, from = -5, to = -1)
 
 ALLCL <- ALL %>% 
   filter(CL != "-")
+
+
+
 
 
 ### Checklist ####
@@ -47,7 +76,10 @@ CheckCL <- Check %>%
   filter(CL..cm. != "na") %>%
   filter(Family == "Testudinidae")
 
-ALL<-read.csv(choose.files(" "), skip = 17,sep=",", header=TRUE) # read csv from line 18 (skip), separated with comma (NOT WORKING)
+# ALL<-read.csv(choose.files(" "), skip = 17,sep=",", header=TRUE) # read csv from line 18 (skip), separated with comma (NOT WORKING)
+
+
+
 
 #try with paleobioDB instead of reading file
 x=read.csv("PBDB_fossil_testudines.csv")
