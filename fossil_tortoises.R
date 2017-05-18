bf8fee8be4f54e9abb2c2b3dfe3df6d952c5b5f2
Joos <Julia.Joos@mfn-berlin.de>
2017-05-18 15:42:39 +0200

    rearrange references, still have to gather all taxa for each publication and locality --> use spread??

--- fossil_tortoises.R
+++ fossil_tortoises.R
@@ -74,7 +74,14 @@ paper <- unique(references$TitleJournal)
 
 Ref <- references %>%
   select(Journal, TitleJournal, note, AuthorYear, RefYear, RefNr, CL, Country, Locality, Latitude, Longitude, Epoch, Taxon, Author, comment, collection) %>%
-  filter(note == "not available")
+  filter(note == "not available") %>%
+  tidyr::unite(Taxa, Taxon, Author, comment, collection, sep="_", remove=FALSE) %>%
+  select(Journal, TitleJournal, note, AuthorYear, RefYear, RefNr, CL, Country, Locality, Latitude, Longitude, Epoch, Taxa) %>%
+  group_by(Jounarl, TitleJournal, AuthorYear, RefYear, RefNr) %>%
+  
+  
+
+#  tidyr::unite(eggs, EggNumber, gen_linien, sep="-", remove=FALSE)
 ### TO DO: gather everything starting with Locality to get actual number of missing papers! ####
 
 
