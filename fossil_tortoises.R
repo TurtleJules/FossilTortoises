07b3773eab1832d49fea0ff2c7328f25ff8e37c2
Joos <Julia.Joos@mfn-berlin.de>
2017-06-19 16:06:09 +0200

    extract clade Testudinidae from tree, next step: add fossils (if possible)

--- fossil_tortoises.R
+++ fossil_tortoises.R
@@ -251,6 +251,9 @@ ggplotly(mapAll) # check if plotly and tidyverse have been loaded
 # turtles$taxon_name
 # unique (turtles$matched_name)
 
+
+###########PHYLOGENY ############
+##till's code
 library(picante)
 tree <- read.tree("tree.txt")
 plot(tree)
@@ -273,3 +276,21 @@ writeNexus(tree, file="tree")
 treetest <- read.nexus("tree")
 plot(treetest)
 
+
+#catalina's code
+library(ape)
+library(phytools)
+# library(phangorn) # to make tree ultramteric
+
+setwd("//naturkundemuseum-berlin.de/MuseumDFSRoot/Benutzer/Julia.Joos/Eigene Dateien/MA/Tortoise_Analyses")
+#read tree
+tree<-read.nexus("tree.nex") #package ape
+plot(tree)
+tree2<-extract.clade(tree,findMRCA(tree,c("Manouria_impressa", "Indotestudo_forstenii"))) #ape
+plot(tree2)
+
+
+#add fossils
+targetNode<-findMRCA(tree2,c("Lamna_ditropis","Isurus_paucus")) #gives common ancestor     #phytools
+tree2<-bind.tip(tree,"Carcharocles_megalodon",where=targetNode,position=0.002,edge.length=0.02) #phytools
+#possition is ma before the node, lenght is how much it lasted
--- tree.R
+++ tree.R
@@ -1,26 +1,31 @@
 library(ape)
-library(geiger)
+#library(geiger)
 library(phytools)
-library(nlme)
-library(evomap)
-library(caper)
-library(adephylo)
+#library(nlme)
+#library(evomap)
+#library(caper)
+#library(adephylo)
 library(phangorn)
-library(l1ou)
-library(geomorph)
-library(bayou)
+#library(l1ou)
+#library(geomorph)
+#library(bayou)
 
-
+setwd("//naturkundemuseum-berlin.de/MuseumDFSRoot/Benutzer/Julia.Joos/Eigene Dateien/MA/Tortoise_Analyses")
 #read tree
-tree<-read.nexus("ConsensusTree_Bayesian_SharkPhylogeny.nex")
+tree<-read.nexus("tree.nex") #package ape
+plot(tree)
 # get the matrix to make it ultrametric
-matrix<-cophenetic.phylo(tree)
-tree<-upgma(matrix)
-#isolate clade you are intersted in
-tree2<-extract.clade(tree,findMRCA(tree,c("Galagoides_demidoff","Microcebus_murinus")))
+matrix<-cophenetic.phylo(tree) #ape
+tree<-upgma(matrix) #phangorn
+#isolate clade you are interested in
+tree2<-extract.clade(tree,findMRCA(tree,c("Manouria_impressa", "Indotestudo_forstenii"))) #ape
 
+#"Pseudemys_texana", "Rhinoclemmys_nasuta"
+#"Platysternon_megacephalum","Geoemyda_japonica"
+
+plot(tree2)
 #add fossils
-targetNode<-findMRCA(tree2,c("Lamna_ditropis","Isurus_paucus")) #gives common ancestor     
-tree2<-bind.tip(tree,"Carcharocles_megalodon",where=targetNode,position=0.002,edge.length=0.02)
-#possition is ma before the node, lenght is how much did it lasted
+targetNode<-findMRCA(tree2,c("Lamna_ditropis","Isurus_paucus")) #gives common ancestor     #phytools
+tree2<-bind.tip(tree,"Carcharocles_megalodon",where=targetNode,position=0.002,edge.length=0.02) #phytools
+#possition is ma before the node, lenght is how much it lasted
 
